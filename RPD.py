4a92d6d52dd6c64105c40cf96806e7c606a2ac6b
shimizu_taira <taira17aug@yahoo.co.jp>
2023-06-15 17:27:12 +0900

    RPD.pyのアップデート
    1. コンストラクター内でcalc_rpdメソッドを呼び出すように変更
    2. 複数のデータ間のrpdを行列で返すrpd_matrixメソッドの追加

--- RPD.py
+++ RPD.py
@@ -9,11 +9,12 @@ import matplotlib.pyplot as plt
 
 
 class RPD:
+
     #RPDクラスの定義
     def __init__(self, data1, data2):
         self.data1 = data1
         self.data2 = data2
-        self.rpd = 0.0
+        self.rpd = self.calc_rpd()
     
     #RPDの計算
     def calc_rpd(self):
@@ -32,11 +33,13 @@ class RPD:
         denominator = self.calc_frobenius_norm(matrix_product1)*self.calc_frobenius_norm(matrix_product2)
     
         #RPDを計算
-        self.rpd = numerator / denominator
+        rpd = numerator / denominator
+
+        return rpd
     
     #引数で与えられた行列を列ごとに標準偏差で割って返す
     def divide_matrix_by_std(self, data):
-        return data / np.std(data, axis=0, ddof=1)
+        return data / np.std(data, axis=0)
 
     #行列のフロベニウスノルムを計算する
     def calc_frobenius_norm(self, data):
@@ -52,8 +55,17 @@ class RPD:
 def rpd(data1, data2):
     #RPDを計算する関数
     rpd = RPD(data1, data2)
-    rpd.calc_rpd()
     return rpd.get_rpd()
+
+
+#複数のデータをまとめてRPDを計算して行列で返す関数
+def rpd_matrix(data_list):
+    rpd_matrix = np.zeros((len(data_list), len(data_list)))
+    for i in range(len(data_list)):
+        for j in range(len(data_list)):
+            rpd_matrix[i][j] = rpd(data_list[i], data_list[j])
+    return rpd_matrix
+
 
     
 if __name__ == '__main__':
@@ -62,8 +74,15 @@ if __name__ == '__main__':
              [100, 920]]
     data2 = [[890, 1000], 
              [1000, 920]]
+    data3 = [[1, 1],
+             [100, 920]]
+    li = [data1, data2, data3]
+
+    li  = np.array(li)
 
     data1 = np.array(data1)
     data2 = np.array(data2)
     print(rpd(data1, data2))
+    print(" ")
+    print(rpd_matrix(li))
     
